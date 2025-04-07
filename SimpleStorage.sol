dd4d9e70383e526d90bfd115de10d646483fd1bf
codewithme224 <codewithme224@gmail.com>
2025-04-07 08:30:52 +0000

    first commit

---
+++ SimpleStorage.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+contract SimpleStorage {
+    // Basic types: boolean, int, uint, address, bytes
+    uint256 myFavoriteNumber;
+
+    // array type: bytes32[], uint256[]
+    // uint256[] listsOfFavoriteNumbers;
+
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // 
+    // Person public emefa = Person({favoriteNumber: 100, name: "Emefa"});
+
+    Person[] public people;
+
+    // modifier: allows you to call a method before or after another method
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    // view, pure: to read states from the blockchain
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
+    }
+
+
+    function addPerson(string memory _name , uint256 _favoriteNumber ) public {
+        people.push(Person({ favoriteNumber: _favoriteNumber, name: _name}));
+    }
+
+}
